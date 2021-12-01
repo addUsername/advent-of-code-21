3be805064c99e607ea324a28c255fc414ccd38c7
addUsername <60299373+addUsername@users.noreply.github.com>
2021-12-01 23:55:35 +0100

    del unnecessary code

--- 1/Day1.py
+++ 1/Day1.py
@@ -8,9 +8,7 @@ steps = [(1 if x[i] > x[i-1] else 0) for i in range (1, len(x))]
 print(sum(steps))
 
 #2
-x.append(0)
-x.append(0)
-x.append(0)
-
-steps2 = [(1 if sum(x[i+1:i+4]) > sum(x[i:i+3]) else 0) for i in range (len(x)-3)]
+# Okay, its seems that you can't get Index Out Of Bounds Exception while slicing in python, feels wrong but ok
+steps2 = [(1 if sum(x[i+1:i+4]) > sum(x[i:i+3]) else 0) for i in range (len(x))]
 print(sum(steps2))
+
