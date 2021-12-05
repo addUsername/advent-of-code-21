aa73f4046466db250a04de4d671e637ca8597663
addUsername <60299373+addUsername@users.noreply.github.com>
2021-12-05 02:11:09 +0100

    Add files via upload

--- 3/Day3.cu
+++ 3/Day3.cu
@@ -1,8 +1,5 @@
-#include <fcntl.h>    /* For O_RDWR */
 #include <unistd.h>
 #include <sys/mman.h>   /* For open(), creat()   */
-//#define _GNU_SOURCE
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -20,9 +17,7 @@ __global__ void cuda_hello(char ** b, char* deviceOutput ){
             sum++;
         }        
     }
-    printf("a\n");
-
-    if (sum > NUMBER_ROWS/2){
+    if (sum >= NUMBER_ROWS/2){
         deviceOutput[threadIdx.x] = '1';
     }else{        
         deviceOutput[threadIdx.x] = '0';
@@ -30,6 +25,79 @@ __global__ void cuda_hello(char ** b, char* deviceOutput ){
     __syncthreads();        
 }
 
+//This makes no sense bc just one thread is needed but for practice purposes it's ok
+__global__ void getOxigen(char ** b, char* deviceOutput, char* oxi){
+
+    const int NUMBER_ROWS = 1001;
+    int LENGHT_ROW = 12+1;
+
+    char *sorted[NUMBER_ROWS];
+    int size = NUMBER_ROWS;
+    int index = 0;
+
+
+    for (int i = 0; i < LENGHT_ROW ; i++){
+        for (int j = 0; j < size- 1; j++){
+
+            if(i == 0){                
+                if(b[j][0] == deviceOutput[0] ){
+                    sorted[index] = (char*) malloc(LENGHT_ROW);                
+                    sorted[index] = b[j];
+                    index++;
+                }
+            }else{
+                if(sorted[j][i] == deviceOutput[i] ){              
+                    sorted[index] = sorted[j];
+                    index++;
+                    
+                }
+            }
+        }
+        size = index;
+        index = 0;       
+    }
+    //strcpy doesn't work here
+    for(int k = 0; k<LENGHT_ROW; k++){
+        oxi[k] = sorted[0][k];
+    }    
+    __syncthreads();        
+}
+
+__global__ void getC02(char ** b, char* deviceOutput, char* co2){
+
+    const int NUMBER_ROWS = 1001;
+    int LENGHT_ROW = 12+1;
+
+    char *sorted[NUMBER_ROWS];
+    int size = NUMBER_ROWS;
+    int index = 0;
+
+    for (int i = 0; i < LENGHT_ROW ; i++){
+        for (int j = 0; j < size- 1; j++){
+
+            if(i == 0){                
+                if(b[j][0] != deviceOutput[0] ){
+                    sorted[index] = (char*) malloc(LENGHT_ROW);                
+                    sorted[index] = b[j];
+                    index++;
+                }
+            }else{
+                if(sorted[j][i] == deviceOutput[i] ){                
+                    sorted[index] = sorted[j];
+                    index++;
+                    
+                }
+            }
+        }       
+        size = index;
+        index = 0;       
+    }
+    //strcpy doesn't work here    
+    for(int k = 0; k<LENGHT_ROW; k++){
+        co2[k] = sorted[0][k];
+    }
+    __syncthreads();        
+}
 
 int main() {
 
@@ -80,15 +148,13 @@ int main() {
     // cudaMempcy or memcpy doesn't do any kind of "deep copying" so, we need to iterate (over *b[]) to allocate and
     // copy each string, while storing its device adresses in a host array (* ptrDevice[]), then copy this array from host memory into device memory.
 
-    // This holds ptr addresses from device (gpu)
-    
+    // This holds ptr addresses from device (gpu)    
     char *ptrDevice[NUMBER_ROWS];
     // This is the argument for the cuda func
     char **_total;
 
     for (int i = 0; i< NUMBER_ROWS -1; i++){
 
-        // cudaError_t is magic | 1) (if err == 0)-> OK | 2) cudaGetErrorString(err) - returns "no error" or error
         // malloc 13 chars size and store its "device" pointer to our host ptrDevice
         cudaError_t err = cudaMalloc((void **)&ptrDevice[i], LENGHT_ROW);
         // _________________________________^_^
@@ -97,8 +163,7 @@ int main() {
             printf("%s",cudaGetErrorString(err));
             return -1;            
         }
-
-        // invalid argument whyyyy       
+    
         // Then we copy strings from *b[] in the gpu address allocated before
         err = cudaMemcpy(ptrDevice[i], b[i], LENGHT_ROW, cudaMemcpyHostToDevice);
         if (err != 0) {
@@ -157,8 +222,6 @@ int main() {
             epsilon[i] = '1';
         }
     }
-    //gamma[NUMBER_ROWS-4] ='\0';
-
 
     printf("\n%s", gamma);                  //Should be 001100001011
     printf("\n%d", strtol(gamma,NULL,2));   //Should be 779 
@@ -167,6 +230,55 @@ int main() {
 
     // I really dont know why i have to do gamma+1 and epsilon-1, some binary shit i hope
     printf("\n %ld", (1 + strtol(gamma,NULL,2)) * ( -1 + strtol(epsilon,NULL,2))); //Should be 2583164
+
+
+    printf("\nSECOND EX");
+    char * oxigenDevice;
+    err = cudaMalloc((void **)&oxigenDevice, LENGHT_ROW);
+    // _____________________^_^
+    if (err != 0) {
+        printf("error allocating");
+        printf("%s",cudaGetErrorString(err));
+        return -1;            
+    }
+
+    getOxigen<<<1,1>>>(_total, deviceOutput, oxigenDevice);
+    cudaDeviceSynchronize();
+    
+    char * oxigen = (char*) malloc(LENGHT_ROW);
+    err = cudaMemcpy(oxigen, oxigenDevice, LENGHT_ROW, cudaMemcpyDeviceToHost);
+    if (err != 0) {
+            printf("error copying oxi ");
+            printf("%s",cudaGetErrorString(err));
+            return -1;            
+    }
+    printf("\n oxigen -> ");
+    printf("%s",oxigen);
+    printf("\n%ld",strtol(oxigen,NULL,2));
+    
+    char * co2Device;
+    err = cudaMalloc((void **)&co2Device, LENGHT_ROW);
+    // _____________________^_^
+    if (err != 0) {
+        printf("error allocating");
+        printf("%s",cudaGetErrorString(err));
+        return -1;
+    }
+    getC02<<<1,1>>>(_total, deviceOutput, co2Device);
+    cudaDeviceSynchronize();
+
+    char * co2 = (char*) malloc(LENGHT_ROW);
+    err = cudaMemcpy(co2, co2Device, LENGHT_ROW, cudaMemcpyDeviceToHost);
+    if (err != 0) {
+            printf("error copying co2 ");
+            printf("%s",cudaGetErrorString(err));
+            return -1;            
+    }
+    printf("\n c02 -> ");
+    printf("%s",co2);    
+    printf("\n%ld",strtol(co2,NULL,2) );
+
+    printf("\n %ld",(1+strtol(oxigen,NULL,2)) * (-1+strtol(co2,NULL,2)));
     // check for null before..
     /*
     cudaFree(ptrDevice);
